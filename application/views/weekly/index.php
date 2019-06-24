c461582e21082a8b379149a7f2d56ef46fa20ba8
Robin Reyes <robin@fullstack.ph>
2019-06-24 23:24:38 +0800

    Team member time availability progress bar

--- application/views/weekly/index.php
+++ application/views/weekly/index.php
@@ -26,10 +26,16 @@
         </div>
         <div class="p15 bg-white pb0">
             <div class="row">
-                <div class="col-md-4">
+                <div class="col-md-12">
                     <button class="btn btn-default" id="reload-kanban-button"><i class="fa fa-refresh"></i></button>
                     <a href="#" class="btn btn-default" title="Import Projects" data-act="ajax-modal" data-title="Import Projects" data-action-url="404"><i class="fa fa-plus-circle"></i> Import Projects</a>
                     <a href="#" class="btn btn-default" title="Import Projects Manually" data-act="ajax-modal" data-title="Import Projects Manually" data-action-url="404"><i class="fa fa-plus-circle"></i> Import Projects Manually</a>
+
+                <?php foreach ($users as $key => $user) { ?>
+                  <span id="user_<?php echo $user->id; ?>" class="avatar-sm avatar pull-right mt-5 mr10 weekly-avatar">
+                    <img alt="<?php echo $user->first_name.' '.$user->last_name; ?>" src="<?php echo ($user->image ? base_url().'files/profile_images/'.$user->image : base_url().'assets/images/avatar.jpg'); ?>">
+                  </span>
+                <?php }?>
                 </div>
             </div>
         </div>
@@ -47,6 +53,27 @@
 
         $userSelect.select2();
         $teamSelect.select2();
+
+        <?php foreach ($users as $key => $user): ?>
+
+        var bar_<?php echo $user->id; ?> = new ProgressBar.Circle(user_<?php echo $user->id; ?>, {
+          color: '#28B513',
+          trailColor: '#eee',
+          trailWidth: 8,
+          duration: 2800,
+          easing: 'easeInOut',
+          strokeWidth: 8,
+          from: {color: '#28B513', a:0},
+          to: {color: '#28B513', a:1},
+          // Set default step function for all animate calls
+          step: function(state, circle) {
+            circle.path.setAttribute('stroke', state.color);
+          }
+        });
+
+        bar_<?php echo $user->id; ?>.animate(0.75);
+
+        <?php endforeach; ?>
     });
 
   </script>
